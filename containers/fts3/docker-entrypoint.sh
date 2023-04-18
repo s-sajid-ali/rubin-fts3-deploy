5e91ca507a506d8c5c959b430c8b0ece96a8d2d1
Brandon White <bjwhite@fnal.gov>
2023-04-18 10:32:56 -0700

    Add Docker stuff required to build an FTS3 image off the SLATE FTS3 base like FNAL.

---
+++ containers/fts3/docker-entrypoint.sh
@@ -0,0 +1,53 @@
+#!/bin/bash -e
+
+if ! whoami &> /dev/null; then
+  if [ -w /etc/passwd ]; then
+    echo "${USER_NAME:-default}:x:$(id -u):0:${USER_NAME:-default} user:${HOME}:/sbin/nologin" >> /etc/passwd
+  fi
+fi
+
+# Mounting the secrets that we have as files
+cp /tmp/fts3-host-pems/hostcert.pem /etc/grid-security/
+chmod 644 /etc/grid-security/hostcert.pem
+chown root:root /etc/grid-security/hostcert.pem
+cp /tmp/fts3-host-pems/hostkey.pem /etc/grid-security/
+chmod 400 /etc/grid-security/hostkey.pem
+chown root:root /etc/grid-security/hostkey.pem
+
+# Update configuration files with the contents of the environment variables
+envsubst < /tmp/fts3-configs/fts3config > /tmp/fts3config.repl
+cp /tmp/fts3config.repl /etc/fts3/fts3config
+
+envsubst < /tmp/fts3-configs/fts-msg-monitoring.conf > /tmp/fts-msg-monitoring.repl
+cp /tmp/fts-msg-monitoring.repl /etc/fts3/fts-msg-monitoring.conf
+
+envsubst < /tmp/fts3-configs/fts3restconfig > /tmp/fts3restconfig.repl
+cp /tmp/fts3restconfig.repl /etc/fts3/fts3restconfig
+
+chown root:apache /etc/fts3web/fts3web.ini
+chown -R fts3:fts3 /var/log/fts3rest
+
+if [[ ! -z "${DATABASE_UPGRADE}" ]]; then
+   yes Y | python /usr/share/fts/fts-database-upgrade.py
+fi
+if [[ ! -z "${REST_HOST}" ]]; then
+   replaceCommand="sed -i -e 's/*/${REST_HOST}/g' /etc/httpd/conf.d/fts3rest.conf"
+   eval $replaceCommand
+fi
+if [[ -z "${WEB_INTERFACE}" ]]; then
+   rm /etc/httpd/conf.d/ftsmon.conf
+else
+   echo "${HOSTNAME} ${WEB_INTERFACE}" > /etc/fts3/host_aliases
+fi
+
+fetch-crl
+fts_server
+fts_qos
+fts_msg_bulk
+httpd
+
+echo 'while true; do sleep 3600; &>/dev/null httpd -k graceful; done' > /root/restart_httpd.sh
+chmod +x /root/restart_httpd.sh
+nohup /root/restart_httpd.sh &
+
+supervisord -c /etc/supervisord.conf --nodaemon
